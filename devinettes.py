597c9d78e428359b376e2a69ee4454bdac59bfc0
Olivier Vezina <1857132@cstjean.qc.ca>
2020-10-15 12:39:22 -0400

    Jeu de base

--- devinettes.py
+++ devinettes.py
@@ -5,14 +5,37 @@
 
     par: Olivier Vezina
 """
-
+from random import randrange
 
 def main():
     """
 
     Fonction principale
     """
-    pass
+    print("Bonjour, je m'appelle Pedro CallMe")
+    print("J'ai choisi un nombre entier entr 1 et 100")
+    print("Pouvez-vous le deviner?")
+    devinette()
+
+
+def devinette():
+    """
+        Fonction pour deviner le nombre
+    """
+    nbRandom = randrange(1, 101)
+    cpt = 0
+    while True:
+        cpt = cpt + 1
+        guess = int(input(f"Essaie {cpt}:"))
+        if guess == nbRandom:
+            print("Bravo, vous avez deviné le nombre")
+            break
+        elif guess < nbRandom:
+            print("Votre nombre est trop petit...")
+            continue
+        else:
+            print("Votre nombre est trop grand...")
+            continue
 
 
 if __name__ == '__main__':
