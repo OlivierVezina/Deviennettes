4c6ede09500aa2561102c12dfec2f6c00ea9117a
Olivier Vezina <1857132@cstjean.qc.ca>
2020-10-15 12:02:01 -0400

    Commencer DEviennette

---
+++ devinettes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+    Jeu de devinette
+
+    par: Olivier Vezina
+"""
+
+
+def main():
+    """
+
+    Fonction principale
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
